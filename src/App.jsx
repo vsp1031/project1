5d0b351d22ee1ee4def197371d01984879bce191
Saakshi Kobarne <saakshik15@gmail.com>
2024-04-02 11:39:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-zinc-900 text-white flex w-full h-screen items-center justify-center'>App</div>
+  )
+}
+
+export default App 
